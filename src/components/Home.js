13d03503b6e0e702cf4327a49467c27c954dd784
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-11-17 19:02:10 +0100

    Add navlinks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,12 @@
+import { NavLink } from "react-router-dom";
+
 export function Home() {
-  return <h1>Homepage</h1>;
+  return (
+    <>
+      <h1>Homepage</h1>
+
+      <NavLink to={"/login"}>Login</NavLink>
+      <NavLink to={"/signup"}>Signup</NavLink>
+    </>
+  );
 }
